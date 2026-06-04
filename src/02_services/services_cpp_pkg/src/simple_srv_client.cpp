ae3e66bbb06f2ef79a11558457fa25ba0f3bea59
pawo90 <pawelp1990@gmail.com>
2026-06-04 22:23:46 +0200

    Add services clients nodes

--- src/02_services/services_cpp_pkg/src/simple_srv_client.cpp
+++ src/02_services/services_cpp_pkg/src/simple_srv_client.cpp
@@ -1,18 +1,68 @@
 #include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/srv/add_two_ints.hpp"
 
-class SimpleServiceClient : public rclcpp::Node // MODIFY NAME
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class SimpleServiceClient : public rclcpp::Node
 {
 public:
-	SimpleServiceClient() : Node("simple_service_client") // MODIFY NAME
-	{
+	SimpleServiceClient() : Node("simple_service_client") 
+	{	
+		// Create a service server
+		//
+		// Parameters:
+		// - Service type: AddTwoInts
+		// - Service name: "/add_ints"
+		client_ = this->create_client<example_interfaces::srv::AddTwoInts>(
+			"/add_ints"
+		);
+	
+		RCLCPP_INFO(this->get_logger(), "Simple C++ service client node has been started");
 	}
+
+	// Sends an async request to the /add_ints service.
+    // Waits until the service is available, then sends a and b.
+    // Response is handled by callbackAddIntsResponse.
+	void callAddInts(int a, int b)
+	{
+		while (! client_->wait_for_service(1s))
+		{
+			RCLCPP_WARN(this->get_logger(), "Waiting for the server...");
+		}
+
+		auto request = std::make_shared<example_interfaces::srv::AddTwoInts::Request>();
+		request->a = a;
+		request->b = b;
+		client_->async_send_request(
+			request,
+			std::bind(&SimpleServiceClient::callbackAddIntsResponse, this, _1)
+		);
+	}
+
 private:
+	// Callback triggered when the service response is received.
+    // Extracts and logs the sum from the response.
+	void callbackAddIntsResponse(
+		rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedFuture future
+	)
+	{
+		auto response = future.get();
+		RCLCPP_INFO(this->get_logger(), "Add ints server respond -> sum is %ld", response->sum);
+	}
+
+	// Service client handle for the '/add_ints' service
+	rclcpp::Client<example_interfaces::srv::AddTwoInts>::SharedPtr client_;
 };
 
 int main(int argc, char **argv)
 {
 	rclcpp::init(argc, argv);
-	auto node = std::make_shared<SimpleServiceClient>(); // MODIFY NAME
+	auto node = std::make_shared<SimpleServiceClient>();
+
+	// Call - Send request to add 2 + 4
+	node->callAddInts(2, 8);
+
 	rclcpp::spin(node);
 	rclcpp::shutdown();
 	return 0;
--- src/02_services/services_py_pkg/services_py_pkg/simple_srv_client.py
+++ src/02_services/services_py_pkg/services_py_pkg/simple_srv_client.py
@@ -1,22 +1,57 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from example_interfaces.srv import AddTwoInts
 
 
 class SimpleServiceClient(Node): 
 	def __init__(self):
 		super().__init__('simple_service_client')
 
-		self.add_ints_service_ = self.create_service()
+		# Create a service client
+        #
+        # Parameters:
+        # - Service type: AddTwoInts
+        # - Service name: "/add_ints"
+		self.client_ = self.create_client(
+			AddTwoInts,
+			"/add_ints"
+		)		
 
 		self.get_logger().info(
             "Simple Python service client node has been started"
         )
 
+    # Sends an async request to the /add_ints service.
+    # Waits until the service is available, then sends a and b.
+    # Response is handled by callbackAddIntsResponse.
+	def callAddInts(self, a: int, b: int):
+		while not self.client_.wait_for_service(1.0):
+			self.get_logger().warn("Waiting for service...")
+		
+		request = AddTwoInts.Request()
+		request.a = a
+		request.b = b
+
+		future = self.client_.call_async(request)
+		future.add_done_callback(
+			self.callbackAddIntsResponse
+		)
+
+    # Callback triggered when the service response is received.
+    # Extracts and logs the sum from the response.
+	def callbackAddIntsResponse(self, future):
+		response = future.result()
+		# response: AddTwoInts.Response = future.result()  # hint for IDE
+		self.get_logger().info(f"Add ints server respond -> sum is {response.sum}")
+
 
 def main(args=None):
 	rclpy.init(args=args)
 	node = SimpleServiceClient()
+
+    # Call - Send request to add 2 + 4
+	node.callAddInts(2, 4)
 
 	rclpy.spin(node)
 	rclpy.shutdown()
